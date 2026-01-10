f6e2db7d83a55d150699ff431c02ebd8a3a574fd
sarah elkholy <saraelkholyarmy03@gmail.com>
2026-01-10 14:53:58 +0200

    - added local notification package - setup local notifications (android/app/build.gradel) - create local_notification_service - init localNotificatioService - handel foreground notifications

--- lib/service/push_notifications_service.dart
+++ lib/service/push_notifications_service.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:push_notifications/service/local_notifications_service.dart';
 
 class PushNotificationsService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
@@ -11,11 +12,18 @@ class PushNotificationsService {
     String? token = await messaging.getToken();
     log(token ?? "null");
     FirebaseMessaging.onBackgroundMessage(handelBackgroundMessage);
+    handelForegroundMessage();
   }
 
   static Future<void> handelBackgroundMessage(RemoteMessage message) async {
     await Firebase.initializeApp();
     // TODO: handel log error
     log(message.notification?.title ?? 'null');
+  }
+
+  static void handelForegroundMessage() {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      LocalNotificationsService.showBasicNotification(message);
+    });
   }
 }
